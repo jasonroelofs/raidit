ae6ade0f6f99ecbb6e9c61261eab55cbf3480cac
Jason Roelofs <jameskilton@gmail.com>
2010-11-02 12:58:21 -0400

    Base rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaidIt::Application.config.secret_token = '694f1dfecffc6eaed94d2f5147a3abf9dad8adf1d90822dafb06ca88d34454c07efa9788f2f31e8ee3a136bed4afc5c0bb041febb99dc660493e987f20d8aa48'
