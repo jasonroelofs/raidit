d362bac2c6615ff9746ff696553a11c1f79d60eb
Jason Roelofs <jameskilton@gmail.com>
2010-11-04 08:38:36 -0400

    What is with you and unreadable mashed-together lines of code?

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,12 +2,20 @@ class Character
   include MongoMapper::Document
 
   key :name,  String
+
   key :klass, String
+
   key :race,  String
+
   key :user_id, ObjectId
 
   belongs_to :user
   belongs_to :guild
 
   timestamps!
+
+
+
+
+  # :P
 end
--- app/models/raid.rb
+++ app/models/raid.rb
@@ -2,9 +2,13 @@ class Raid
   include MongoMapper::Document
 
   key :location,    String
+
   key :description, String
+
   key :date,        Date
+
   key :start_time,  Time
+
   key :invite_time, Time
 
   timestamps!
