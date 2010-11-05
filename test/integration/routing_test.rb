a95e7f44a4c4434be81f67acd3c22148be83edd9
Jason Roelofs <jameskilton@gmail.com>
2010-11-05 14:04:17 -0400

    test the route path itself that I want

--- test/integration/routing_test.rb
+++ test/integration/routing_test.rb
@@ -4,8 +4,6 @@ class RoutingTest < ActionDispatch::IntegrationTest
 
   test "routes exist" do
     assert_routing "/", :controller => "calendar", :action => "show"
-
-    assert_routing(raids_url, {:controller => "raids", :action => "index"})
-    assert_routing(raid_url(14), {:controller => "raids", :action => "show", :id => "14"})
+    assert_routing "/raids", :controller => "raids", :action => "index"
   end
 end
