2bbe7aa238b06d01ec1c0e20ff29e743f51e4db0
Jason Roelofs <jameskilton@gmail.com>
2010-11-16 13:08:42 -0500

    Force timezone to be Central where our server is

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,18 @@ class ApplicationController < ActionController::Base
   before_filter :set_current_guild
   before_filter :set_current_user
 
+  before_filter :set_time_zone
+
   def current_guild
     Guild.current
   end
   helper_method :current_guild
 
   protected
+
+  def set_time_zone
+    Time.zone = "Central Time (US & Canada)"
+  end
 
   def set_current_guild
     Guild.current = "Exiled"
