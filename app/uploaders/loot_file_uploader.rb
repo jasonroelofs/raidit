32c3eb515b18f5dfebf4ecc8de973e51703f1694
Jason Roelofs <jameskilton@gmail.com>
2011-01-08 15:06:34 -0500

    Fixed width and date display on loot history

--- app/uploaders/loot_file_uploader.rb
+++ app/uploaders/loot_file_uploader.rb
@@ -15,20 +15,4 @@ class LootFileUploader < CarrierWave::Uploader::Base
     Rails.root.join("tmp", "loot_files")
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
-
-  # Override the filename of the uploaded files:
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
-
 end
