bd5586c05a6bfde08941f68da223aff26481a6b6
Jason Roelofs <jameskilton@gmail.com>
2011-01-08 14:55:08 -0500

    Proper styling of loot links

--- app/helpers/loot_helper.rb
+++ app/helpers/loot_helper.rb
@@ -6,7 +6,7 @@ module LootHelper
     item_name = text.match(/(\[.*\])/)
 
     if item_id && item_name
-      link = link_to(item_name[1], "http://www.wowhead.com/?item=#{item_id[1]}")
+      link = link_to(item_name[1], "http://www.wowhead.com/?item=#{item_id[1]}", :class => "epic")
       text.gsub(/\|c.*\|r/, link)
     else
       text
