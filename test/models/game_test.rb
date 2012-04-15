3a156e1f168b7b7b25a33b04390dfd8ac76d9858
Jason Roelofs <jameskilton@gmail.com>
2012-04-15 15:59:05 -0400

    Add #name and #slug to Game

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -3,8 +3,20 @@ require 'models/game'
 
 describe Game do
 
+  before do
+    @game = Game.new("World of Warcraft", "wow")
+  end
+
   it "exists" do
-    Game.new.wont_be_nil
+    @game.wont_be_nil
+  end
+
+  it "has a full name" do
+    @game.name.must_equal "World of Warcraft"
+  end
+
+  it "has a short slug" do
+    @game.slug.must_equal "wow"
   end
 
 end
