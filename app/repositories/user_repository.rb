27201d4bd1006c1f93238a11a91c0a2d7001c551
Jason Roelofs <jameskilton@gmail.com>
2012-04-25 16:28:09 -0500

    Update UserRepository to be able to save a User

--- app/repositories/user_repository.rb
+++ app/repositories/user_repository.rb
@@ -6,6 +6,10 @@ class UserRepository
       data_store.users
     end
 
+    def save(user)
+      data_store.save user
+    end
+
     # See GuildRepository.data_store
     def data_store
       Repository.store
--- test/repositories/user_repository_test.rb
+++ test/repositories/user_repository_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 require 'repositories/user_repository'
+require 'models/user'
 
 describe UserRepository do
   it "exists" do
@@ -9,6 +10,15 @@ describe UserRepository do
   describe ".all" do
     it "returns the full list of users" do
       UserRepository.all.must_equal []
+    end
+  end
+
+  describe ".save" do
+    it "sends a user object into the data store" do
+      user = User.new email: "email@me.com"
+      UserRepository.save user
+
+      UserRepository.all.must_equal [user]
     end
   end
 end
