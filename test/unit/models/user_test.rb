2581ddda3338c986bbdd4d0ff27c680149c1ebdd
Jason Roelofs <jameskilton@gmail.com>
2012-05-18 16:20:12 -0400

    Add web session token to User

--- test/unit/models/user_test.rb
+++ test/unit/models/user_test.rb
@@ -10,4 +10,10 @@ describe User do
     u = User.new email: "email"
     u.email.must_equal "email"
   end
+
+  it "has a web session token" do
+    u = User.new
+    u.web_session_token = "session token"
+    u.web_session_token.must_equal "session token"
+  end
 end
