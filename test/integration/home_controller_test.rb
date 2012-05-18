b8e0f17cfdb442ec64357ca2bcb5e4f6c64c971f
Jason Roelofs <jameskilton@gmail.com>
2012-05-18 14:53:52 -0400

    Add Home controller, the landing point for the application

---
+++ test/integration/home_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionController::TestCase
+  tests HomeController
+
+  describe "#index" do
+    it "renders the home page" do
+      get :index
+      @response.status.must_equal 200
+    end
+  end
+
+end
