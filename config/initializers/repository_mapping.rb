e95dd786e0ceab1665c17a244e4896622d23737a
Jason Roelofs <jasongroelofs@gmail.com>
2012-05-22 21:03:18 -0400

    Ensure repo mappings survive dev reloads

--- config/initializers/repository_mapping.rb
+++ config/initializers/repository_mapping.rb
@@ -1,10 +1,12 @@
 require 'repositories/in_memory'
 
-Repository.configure(
-  "User" => InMemory::UserRepo.new,
-  "Guild" => InMemory::GuildRepo.new,
-  "Character" => InMemory::CharacterRepo.new,
-  "Raid" => InMemory::RaidRepo.new,
-  "Signup" => InMemory::SignupRepo.new,
-  "Permission" => InMemory::PermissionRepo.new
-)
+ActiveSupport.on_load(:after_initialize) do
+  Repository.configure(
+    "User" => InMemory::UserRepo.new,
+    "Guild" => InMemory::GuildRepo.new,
+    "Character" => InMemory::CharacterRepo.new,
+    "Raid" => InMemory::RaidRepo.new,
+    "Signup" => InMemory::SignupRepo.new,
+    "Permission" => InMemory::PermissionRepo.new
+  )
+end
