4d57f07434e580fc1a2f1b12550339dd94c2194c
Jason Roelofs <jasongroelofs@gmail.com>
2012-05-22 21:12:22 -0400

    Update repository mapping to not be lost in dev reloading

--- config/initializers/repository_mapping.rb
+++ config/initializers/repository_mapping.rb
@@ -1,12 +1,16 @@
-require 'repositories/in_memory'
+require_dependency 'repositories/in_memory'
 
-ActiveSupport.on_load(:after_initialize) do
-  Repository.configure(
-    "User" => InMemory::UserRepo.new,
-    "Guild" => InMemory::GuildRepo.new,
-    "Character" => InMemory::CharacterRepo.new,
-    "Raid" => InMemory::RaidRepo.new,
-    "Signup" => InMemory::SignupRepo.new,
-    "Permission" => InMemory::PermissionRepo.new
-  )
+Raidit::Application.configure do
+
+  config.to_prepare do
+    Repository.configure(
+      "User" => InMemory::UserRepo.new,
+      "Guild" => InMemory::GuildRepo.new,
+      "Character" => InMemory::CharacterRepo.new,
+      "Raid" => InMemory::RaidRepo.new,
+      "Signup" => InMemory::SignupRepo.new,
+      "Permission" => InMemory::PermissionRepo.new
+    )
+  end
+
 end
