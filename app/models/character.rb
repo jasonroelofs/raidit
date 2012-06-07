0093bfcae76a963f9306b3278cf463dd97301ebb
Jason Roelofs <jasongroelofs@gmail.com>
2012-06-06 22:16:51 -0400

    Add a few more attributes to Character

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,9 +1,12 @@
 class Character
 
-  attr_reader :name, :user
+  attr_reader :name, :user, :game, :server, :region
 
   def initialize(attrs = {})
     @name = attrs[:name]
     @user = attrs[:user]
+    @game = attrs[:game]
+    @server = attrs[:server]
+    @region = attrs[:region]
   end
 end
--- test/unit/models/character_test.rb
+++ test/unit/models/character_test.rb
@@ -9,8 +9,12 @@ describe Character do
 
   it "takes attributes in a hash" do
     u = User.new
-    c = Character.new name: "Weemuu", user: u
+    c = Character.new name: "Weemuu", user: u, game: "game", server: "Detheroc",
+      region: "US"
     c.name.must_equal "Weemuu"
     c.user.must_equal u
+    c.game.must_equal "game"
+    c.server.must_equal "Detheroc"
+    c.region.must_equal "US"
   end
 end
