18de84adddc61b8d6400b388c58f7b520e8a5ea3
Jason Roelofs <jasongroelofs@gmail.com>
2012-06-09 16:26:04 -0400

    Add start and invite times to Raid. Location => Where

--- app/models/raid.rb
+++ app/models/raid.rb
@@ -1,6 +1,8 @@
 class Raid
 
-  attr_reader :id, :when, :leader, :owner
+  attr_reader :id, :when, :leader, :owner, :where
+
+  attr_reader :start_at, :invite_at
 
   attr_accessor :groups
 
@@ -9,6 +11,9 @@ class Raid
     @when = attrs[:when]
     @leader = attrs[:leader]
     @owner = attrs[:owner]
+    @where = attrs[:where]
+    @start_at = attrs[:start_at]
+    @invite_at = attrs[:invite_at]
     @groups = attrs[:groups] || []
   end
 end
