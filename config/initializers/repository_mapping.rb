86031f55cd4123b859ea8f897a78fd201841c4c0
Jason Roelofs <jasongroelofs@gmail.com>
2012-06-24 17:09:39 -0400

    Add ids to our Raids
    This, along with the ActiveModel integration being added next, will
    allow Raid objects to work inside of Rails like any normal ActiveRecord
    object.

--- config/initializers/repository_mapping.rb
+++ config/initializers/repository_mapping.rb
@@ -23,15 +23,20 @@ Raidit::Application.configure do
           :region => "US", :user => jason)
       )
 
+      id = 1
+
       (1.week.ago.to_date..2.weeks.from_now.to_date).each do |day|
         raid = Raid.new :when => day, :owner => jason, :where => "ICC",
           :start_at => Time.parse("20:00"), :invite_at => Time.parse("19:30")
 
+        raid.id = id
         raid.set_role_limit :tank, (rand * 5).to_i
         raid.set_role_limit :dps, (rand * 20).to_i
         raid.set_role_limit :healer, (rand * 5).to_i
 
         Repository.for(Raid).save(raid)
+
+        id += 1
       end
     end
   end
