3a836d7df614968b0dce70197da0f861e14badb0
Jason Roelofs <jasongroelofs@gmail.com>
2012-07-25 21:55:12 -0400

    Fix sign-in step to use the username arg

--- features/step_definitions/session_steps.rb
+++ features/step_definitions/session_steps.rb
@@ -1,7 +1,7 @@
-Given /^I am signed in as "(.*?)"$/ do |arg1|
+Given /^I am signed in as "(.*?)"$/ do |username|
   steps %{
     When I visit /login
-    Then I fill in "jason" for "login"
+    Then I fill in "#{username}" for "login"
     And I fill in "password" for "password"
     And I press "Log In"
   }
