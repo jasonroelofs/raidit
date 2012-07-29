6f4facbffa3e9f7c2169c5f6847992f101cf0815
Jason Roelofs <jasongroelofs@gmail.com>
2012-07-29 18:18:31 -0400

    Allow assigning a character to a guild in cucumber

--- features/step_definitions/character_steps.rb
+++ features/step_definitions/character_steps.rb
@@ -2,6 +2,7 @@ Given /^"(.*?)" has the following characters$/ do |login, table|
   repo = Repository.for(Character)
   current_user = Repository.for(User).find_by_login(login)
 
+
   table.hashes.each do |row|
     c = Character.new(
       region: row[:region],
@@ -10,6 +11,11 @@ Given /^"(.*?)" has the following characters$/ do |login, table|
       name: row[:name],
       user: current_user
     )
+
+    if row[:guild]
+      guild = FindGuild.by_name row[:guild]
+      c.guild = guild
+    end
 
     repo.save c
   end
