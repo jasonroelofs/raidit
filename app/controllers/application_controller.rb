143573e68b7ea900b7d9d0e861271879a474dadc
Jason Roelofs <jasongroelofs@gmail.com>
2012-08-04 15:35:28 -0400

    Pull AccessControl code into ApplicationController proper
    There's no reason for the module indirection right now. If the amount of
    methods on ApplicationController gets out of hand I may revisit this or
    consider something else to solve the problem.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,27 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper :all
 
-  include AccessControl
+  class << self
+    ##
+    # Flag the actions that require a current user
+    ##
+    def requires_user(options = {})
+      before_filter :require_user, options
+    end
+  end
 
+  ##
+  # Returns the currently logged in user
+  ##
   def current_user
     @current_user ||= find_logged_in_user
   end
   helper_method :current_user
 
+  ##
+  # Returns the currently selected guild of the currently logged
+  # in user
+  ##
   def current_guild
     if current_user
       @current_guild || find_current_guild
@@ -16,12 +30,20 @@ class ApplicationController < ActionController::Base
   end
   helper_method :current_guild
 
+  ##
+  # Check if the current user has the given permission set
+  # for the currently selected guild
+  ##
   def current_user_has_permission?(permission)
     CheckUserPermissions.new(current_user, current_guild).allowed?(permission)
   end
   helper_method :current_user_has_permission?
 
   protected
+
+  def require_user
+    redirect_to login_path unless current_user
+  end
 
   def find_logged_in_user
     action = FindUser.new
