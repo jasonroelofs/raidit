927c9ca5634e0f9e7fbc1e262ef49974f0b4be6d
Jason Roelofs <jasongroelofs@gmail.com>
2012-08-15 21:31:38 -0400

    Refactor UpdateUser's password handling

--- app/interactors/update_user.rb
+++ app/interactors/update_user.rb
@@ -11,13 +11,9 @@ class UpdateUser
     @current_user.login = params[:login] if params[:login].present?
     @current_user.email = params[:email] if params[:email].present?
 
-    if params[:current_password].present? &&
-      params[:new_password].present? &&
-      params[:confirm_new_password].present?
-
-      if @current_user.password == params[:current_password] &&
-        params[:new_password] == params[:confirm_new_password]
-
+    if password_change_requested?(params)
+      if current_password_matches?(params) &&
+        new_passwords_match?(params)
         @current_user.password = params[:new_password]
       else
         return false
@@ -26,5 +22,21 @@ class UpdateUser
 
     Repository.for(User).save(@current_user)
     true
+  end
+
+  protected
+
+  def password_change_requested?(params)
+    params[:current_password].present? &&
+      params[:new_password].present? &&
+      params[:confirm_new_password].present?
+  end
+
+  def current_password_matches?(params)
+    @current_user.password == params[:current_password]
+  end
+
+  def new_passwords_match?(params)
+    params[:new_password] == params[:confirm_new_password]
   end
 end
