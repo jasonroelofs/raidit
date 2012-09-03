2163e97300a41f348cec5f785888db6e80aef11b
Jason Roelofs <jasongroelofs@gmail.com>
2012-09-03 15:05:52 -0400

    Add format that displays timezone

--- config/initializers/date_and_time_formats.rb
+++ config/initializers/date_and_time_formats.rb
@@ -1,5 +1,6 @@
 Date::DATE_FORMATS[:full] = "%B %e, %Y"
 Time::DATE_FORMATS[:with_am_pm] = "%l:%M %p"
+Time::DATE_FORMATS[:with_am_pm_tz] = "%l:%M %p %Z"
 
 Time::DATE_FORMATS[:ymd] = "%Y/%m/%d"
 Date::DATE_FORMATS[:ymd] = "%Y/%m/%d"
