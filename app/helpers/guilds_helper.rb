0b5277854cc87d63618bb601b7e6fe3f36214bc7
Jason Roelofs <jasongroelofs@gmail.com>
2012-09-18 22:00:07 -0400

    Add helper for listing the available guilds that aren't the current guild

--- app/helpers/guilds_helper.rb
+++ app/helpers/guilds_helper.rb
@@ -1,5 +1,11 @@
 module GuildsHelper
 
+  def available_guilds
+    ListGuilds.by_user(current_user).reject do |guild|
+      guild == current_guild
+    end
+  end
+
   # NOTE Not currently used, but do want to have your current guilds
   # pre-populated in the Guild Select box, just need to figure out how
   # to make select2 do this.
--- test/unit/helpers/guilds_helper_test.rb
+++ test/unit/helpers/guilds_helper_test.rb
@@ -13,6 +13,23 @@ describe GuildsHelper do
     @user ||= User.new
   end
 
+  def current_guild
+    @current_guild
+  end
+
+  describe "#available_guilds" do
+    it "returns all guilds available to the current user that aren't the current guild" do
+      guild1 = Guild.new
+      guild2 = Guild.new
+      guild3 = Guild.new
+
+      @current_guild = guild2
+      ListGuilds.expects(:by_user).with(current_user).returns([guild1, guild2, guild3])
+
+      available_guilds.must_equal [guild1, guild3]
+    end
+  end
+
   describe "#guild_selection_list" do
 
     it "finds all the current user's guilds and sets up the selector" do
