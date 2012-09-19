68d1eef20813fd19afa9af9e6b02a4f6e7e5808b
Jason Roelofs <jasongroelofs@gmail.com>
2012-09-18 23:14:07 -0400

    Move current guild chooser into a shared partial / helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def current_guild_chooser
+    if current_user && current_guild
+      render "shared/current_guild_chooser"
+    end
+  end
+
   def normalize_name(string_to_normalize)
     (string_to_normalize || "").underscore.gsub(/[_\s]/, '')
   end
