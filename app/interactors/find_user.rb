05a6ced26c445e99a354a797f2544baa856f54c6
Jason Roelofs <jasongroelofs@gmail.com>
2012-09-23 21:42:47 -0400

    Find a user in context of a guild

--- app/interactors/find_user.rb
+++ app/interactors/find_user.rb
@@ -11,4 +11,14 @@ class FindUser
     Repository.for(User).find_by_login_token type, token if token
   end
 
+  def self.by_guild_and_id(guild, user_id)
+    user = Repository.for(User).find(user_id)
+
+    if Repository.for(Character).find_main_character(user, guild)
+      user
+    else
+      nil
+    end
+  end
+
 end
--- test/unit/interactors/find_user_test.rb
+++ test/unit/interactors/find_user_test.rb
@@ -1,10 +1,12 @@
 require 'unit/test_helper'
 require 'models/user'
+require 'models/guild'
+require 'models/character'
 require 'interactors/find_user'
 
 describe FindUser do
 
-  describe "#by_login_token" do
+  describe ".by_login_token" do
     it "can find a user by a login token of the given type" do
       user = User.new
       user.set_login_token :web, "token"
@@ -40,13 +42,46 @@ describe FindUser do
     end
   end
 
-  describe "#by_login" do
+  describe ".by_login" do
     it "finds a user by login" do
       user = User.new login: "markus"
       Repository.for(User).save user
 
       FindUser.by_login("markus").must_equal user
     end
+  end
+
+  describe ".by_guild_and_id" do
+    before do
+      @user1 = User.new id: 1
+      @user2 = User.new id: 2
+
+      @guild1 = Guild.new id: 1
+      @guild2 = Guild.new id: 2
+      @guild3 = Guild.new id: 3
+
+      @char1 = Character.new user: @user1, guild: @guild1, :is_main => true
+      @char2 = Character.new user: @user2, guild: @guild2, :is_main => true
+
+      Repository.for(User).save(@user1)
+      Repository.for(User).save(@user2)
+
+      Repository.for(Character).save(@char1)
+      Repository.for(Character).save(@char2)
+    end
+
+    it "finds the user who's a member of the given guild" do
+      FindUser.by_guild_and_id(@guild1, 1).must_equal @user1
+    end
+
+    it "returns nil if no such user" do
+      FindUser.by_guild_and_id(@guild1, 4).must_be_nil
+    end
+
+    it "returns nil if user isnt in the guild" do
+      FindUser.by_guild_and_id(@guild1, 2).must_be_nil
+    end
+
   end
 
 end
