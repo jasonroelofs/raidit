f5ebd37ad894597d71559b16ca190d1bbc98ecbd
Jason Roelofs <jasongroelofs@gmail.com>
2012-11-02 22:33:14 -0600

    Add notice flash message rendering
    Consolidated login error rendering to just work with :error.

--- app/helpers/flash_messages_helper.rb
+++ app/helpers/flash_messages_helper.rb
@@ -1,15 +1,21 @@
 module FlashMessagesHelper
 
   def render_flash_messages
-    if flash[:login_error]
-      error "Unable to log you in with those credentials"
-    end
+    flash.now[:error] = "Unable to log you in with those credentials" if flash[:login_error]
+
+    [ error(flash[:error]), notice(flash[:notice]) ].flatten.join(" ").html_safe
   end
 
   def error(message)
     content_tag :div, :class => "alert alert-error" do
       message
-    end
+    end if message.present?
+  end
+
+  def notice(message)
+    content_tag :div, :class => "alert alert-info" do
+      message
+    end if message.present?
   end
 
 end
