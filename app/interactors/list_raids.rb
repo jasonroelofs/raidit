2b79d1e44ed9d349a5b7deecbce477b893b8e336
Jason Roelofs <jasongroelofs@gmail.com>
2012-12-28 14:19:47 -0500

    Don't pass nil when we have a valid method that can be called
    We shouldn't have implementation details leaking down into other
    systems, in this case "nil" for date meant "show all". We have the
    Repository api for this case already.

--- app/interactors/list_raids.rb
+++ app/interactors/list_raids.rb
@@ -4,12 +4,17 @@
 class ListRaids
 
   def self.for_guild(guild)
-    for_guild_on_date(guild, nil)
+    sort_newest_first Repository.for(Raid).find_raids_for_guild(guild)
   end
 
   def self.for_guild_on_date(guild, date)
-    Repository.for(Raid).find_raids_for_guild_and_day(guild, date).
-      sort {|r1, r2| r2.when <=> r1.when }
+    sort_newest_first Repository.for(Raid).find_raids_for_guild_and_day(guild, date)
+  end
+
+  protected
+
+  def self.sort_newest_first(list)
+    list.sort {|r1, r2| r2.when <=> r1.when }
   end
 
 end
